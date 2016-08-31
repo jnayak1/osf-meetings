c9f65910eab2bc41b768750cb04f7bba4aeccb41
Jesse Nayak <jdn4ae@virginia.edu>
2016-08-31 11:13:41 -0400

    Fix jshint error

--- app/routes/login.js
+++ app/routes/login.js
@@ -6,7 +6,7 @@ default Ember.Route.extend({
     beforeModel: function() {
         var refererUrl = window.location.toString();
         var nextQueryParam = '';
-        if (refererUrl.indexOf(config.providers.osfMeetings.loginUrl) == -1) {
+        if (refererUrl.indexOf(config.providers.osfMeetings.loginUrl) === -1) {
             // Make sure redirect url doesn't go back to login page
             // and cause infinite loop
             nextQueryParam = "?next=" + window.location;
